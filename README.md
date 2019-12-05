fa2e8841d518c58bd14cea6f38a4bf5861a64d42
Sunjc <sun-coco925>
2019-12-05 15:49:31 +0800

    initial

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Demo project for Spring Boot
+# Demo Project for Spring Boot, Spring Security OAuth 2.0 and WeChat Webpage Authorization
 
 This code is used in the blog "[Integrating Angular 8 with Spring Boot 2, Spring Security, JWT and CORS](http://blog.51cto.com/7308310/2072364)"
--- src/main/java/org/itrunner/wechat/config/SpringSecurityAuditorAware.java
+++ src/main/java/org/itrunner/wechat/config/SpringSecurityAuditorAware.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of(OAuth2Context.getOpenId());
+        return Optional.of(OAuth2Context.getPrincipalName());
     }
 }
